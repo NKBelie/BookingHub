18915643a9a2d91db7745f12d60eff22812e545b
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-06-17 12:14:10 +0200

    Import Footer and Header

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,15 @@
-export default function Layout() {
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
 
+export default function Layout() {
+    return (
+        <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">
+            <Outlet />
+        </main>
+        <Footer />
+        </div>
+    )
 }
