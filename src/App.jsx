134d3dfeb182a5757967fd12947e84ad94e5e1db
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-06-17 11:56:21 +0200

    Add routes for navigations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignupPage from './pages/SignupPage.jsx'
+
 export default function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />} >
+            <Route index element={<HomePage />} />
+          </Route>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
