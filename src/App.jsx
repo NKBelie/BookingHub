b5ffd90ac3a6d4f597896a8822b44fbb018dc7e1
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-06-18 09:32:23 +0200

    Update App.jsx imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout.jsx'
-import HomePage from './pages/HomePage.jsx'
-import LoginPage from './pages/LoginPage.jsx'
-import SignupPage from './pages/SignupPage.jsx'
+//import { SearchProvider } from './components/context/SearchContext'
+import Layout from './components/Layout'
+import HomePage from './pages/HomePage'
+import SearchPage from './pages/SearchPage'
+import PropertyDetailPage from './pages/PropertyDetailPage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 
 export default function App() {
   return (
-      <BrowserRouter>
+    <BrowserRouter>
         <Routes>
-          <Route element={<Layout />} >
+          <Route element={<Layout />}>
             <Route index element={<HomePage />} />
+            <Route path="search" element={<SearchPage />} />
+            <Route path="property/:id" element={<PropertyDetailPage />} />
           </Route>
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignupPage />} />
         </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
   )
 }
